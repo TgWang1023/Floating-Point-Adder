3a6aed034dd896fe8bb16e697d8f3c30388e8137
Tiange Wang <wtgkevin@gmail.com>
2019-02-04 17:49:59 -0800

    clear description

--- add.asm
+++ add.asm
@@ -14,9 +14,9 @@ main:
     sw $s5, 20($sp)
     sw $ra, 24($sp)
     la $t0, A
-    lw $a0, 0($t0)
+    lw $a0, 0($t0) # num1 argument
     la $t1, B
-    lw $a1, 0($t1)
+    lw $a1, 0($t1) # num2 argument
     jal MYADD
     la $t2, C
     sw $v0, 0($t2) # $v0 = result
@@ -24,15 +24,21 @@ main:
 MYADD:
     move $s0, $a0
     move $s1, $a1
+    # exponents
     srl $t0, $s0, 23
-    andi $s2, $t0, 0x00FF # $s2 = num1 exponent
+    andi $s2, $t0, 0x00FF # $s2 = num1 exponent, final result exponent
     srl $t0, $s1, 23
     andi $s3, $t0, 0x00FF # $s3 = num2 exponent
+    # mantissas
+    li $t1, 0x00800000 # bit 24 is 1
     sll $t0, $s0, 9
-    srl $s4, $t0, 9 # $s4 = num1 mantissa
+    srl $s4, $t0, 9 
+    or $s4, $s4, $t1 # $s4 = num1 mantissa, bit24 = hidden 1, bit25 = potential overflow
     sll $t0, $s1, 9
-    srl $s5, $t0, 9 # $s5 = num2 mantissa
-    srl $s0, $s0, 31 # $s0 = num1 sign
+    srl $s5, $t0, 9 
+    or $s5, $s5, $t1 # $s5 = num2 mantissa , bit24 = hidden 1, bit25 = potential overflow
+    # signs
+    srl $s0, $s0, 31 # $s0 = num1 sign, final result sign
     srl $s1, $s1, 31 # $s1 = num2 sign
     blt	$s2, $s3, move_num_one
 move_num_two: # $s2 >= $s3
